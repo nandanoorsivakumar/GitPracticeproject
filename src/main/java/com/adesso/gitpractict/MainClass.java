f33f2d32d730b3d8d56934a6c35c4bb6add298be
Sivakumar <nandanoorsivakumar@gmail.com>
2026-06-13 00:40:02 +0530

    Added method Multiply Version 3

--- src/main/java/com/adesso/gitpractict/MainClass.java
+++ src/main/java/com/adesso/gitpractict/MainClass.java
@@ -3,12 +3,14 @@ package com.adesso.gitpractict;
 public class MainClass {
 
     public static void main(String[] args){
-        System.out.println("Git Version1");
+        System.out.println("Git Version 3");
 
         Calculator calculator = new Calculator();
 
         System.out.println("Addition   ::   "+calculator.add(20,40));
 
         System.out.println("Subtraction   ::   "+calculator.sub(40, 20));
+
+        System.out.println("Subtraction   ::   "+calculator.multiply(40, 20));
     }
 }
